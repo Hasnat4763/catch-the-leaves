53d70ae87565b3fbcfe1e131004941918ff84f77
Hasnat4763 <abulhasnat5737@gmail.com>
2025-10-26 22:11:52 +0600

    Fixed some bugs

--- scripts/catchingnet.gd
+++ scripts/catchingnet.gd
@@ -1,12 +1,6 @@
 extends CharacterBody2D
-
-
 var SPEED = 300.0
 var game_running = false
-
-
-
-
 func _physics_process(_delta: float):
 	var direction_horiz := Input.get_axis("ui_left", "ui_right")
 	var direction_vert := Input.get_axis("ui_up", "ui_down")
@@ -15,10 +9,8 @@ func _physics_process(_delta: float):
 			velocity.x = direction_horiz * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			
 		if direction_vert:
 			velocity.y = direction_vert * SPEED
 		else:
 			velocity.y = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
--- scripts/game.gd
+++ scripts/game.gd
@@ -10,10 +10,16 @@ var stick_timer: float = 0
 var leaf_speed = 150
 var acorn_speed = 200
 var time_left: float
-var game_won: bool
+var game_won: bool = false
 var custom_game: bool = false
 var stick_collected: int = 0
+var base_leaf_speed = 150
+var base_acorn_speed = 200
+var base_net_speed = 0
 const save_path = "user://score_save.save"
+const MAX_LEAF_SPEED = 400
+const MAX_ACORN_SPEED = 500
+const MAX_NET_SPEED = 700
 @export var leaf_scene: PackedScene = preload("res://tscn/leafs.tscn")
 @export var acorn_scene: PackedScene = preload("res://tscn/acorn.tscn")
 @export var stick_scene: PackedScene = preload("res://tscn/stick.tscn")
@@ -25,6 +31,10 @@ const save_path = "user://score_save.save"
 
 func _ready() -> void:
 	randomize()
+	score_target = 20
+	time_limit = 60
+	time_left = time_limit
+	base_net_speed = $catchingnet.SPEED
 	if not DisplayServer.is_touchscreen_available():
 		print("No TouchScreen")
 		$touch_control.hide()
@@ -36,34 +46,55 @@ func _ready() -> void:
 	$catchingnet.hide()
 	$startscreen.show()
 	load_score()
+	game_won = false
 
 func new_game():
 	game_running = false
 	game_over = false
-	if not custom_game:
-		score_target = randi_range(10, 60)
-		time_limit = randf_range(30, 240)
-		time_left = time_limit
+	score_target = randi_range(10, 60)
+	time_limit = randf_range(30, 240)
+	time_left = time_limit
+	score = 0
+	stick_collected = 0
+	custom_game = false
+	leaf_speed = base_leaf_speed
+	acorn_speed = base_acorn_speed
+	$catchingnet.SPEED = base_net_speed
+	spawning_interval = 2.5
+	acorn_spawning_interval = 9.5
+	stick_spawning_interval = 2
 	$endscreen.hide()
 	$overlay/score.text = "Score: 0"
-	$overlay/time_left.text = "Time Left: " + str(time_left)
+	$overlay/time_left.text = "Time Left: " + str(int(time_left)) + " seconds"
 	$overlay/target.text = "Target: " + str(score_target)
+	$overlay/sticks.text = "Sticks: 0/5"
 	$overlay.hide()
 	$catchingnet.hide()
 	$startscreen.show()
+	game_won = false
 
 func start_game():
+	score = 0
+	stick_collected = 0
+	time_left = time_limit
+	game_won = false
 	game_running = true
 	game_over = false
+	leaf_speed = base_leaf_speed
+	acorn_speed = base_acorn_speed
+	$catchingnet.SPEED = base_net_speed
+	spawning_interval = 2.5
+	acorn_spawning_interval = 9.5
 	$bg.play()
 	$catchingnet.show()
 	$overlay.show()
+	$overlay/sticks.text = "Sticks: 0/5"
 	$startscreen.hide()
 
 func start_custom(time_limit_custom: float, target_custom: int, fall_speed_custom: float, movement_speed_custom: float):
-	leaf_speed += fall_speed_custom * 10
-	acorn_speed += fall_speed_custom * 10
-	$catchingnet.SPEED += movement_speed_custom * 10
+	leaf_speed = base_leaf_speed + fall_speed_custom * 10
+	acorn_speed = base_acorn_speed + fall_speed_custom * 10
+	$catchingnet.SPEED = base_net_speed + movement_speed_custom * 10
 	score_target = target_custom
 	time_limit = time_limit_custom
 	time_left = time_limit
@@ -73,6 +104,7 @@ func start_custom(time_limit_custom: float, target_custom: int, fall_speed_custo
 	$overlay/score.text = "Score: 0"
 	$overlay/time_left.text = "Time Left: " + str(int(time_left)) + " seconds"
 	$overlay/target.text = "Target: " + str(score_target)
+	$overlay/sticks.text = "Sticks: 0/5"
 	$startscreen.hide()
 	$overlay.show()
 	$catchingnet.show()
@@ -91,8 +123,8 @@ func stop_game():
 	$endscreen.show()
 	if score > high_score:
 		high_score = score
-		$endscreen/score.text = "Your Score Was " + str(score) + "\n" + "High Score Was " + str(high_score)
 		save()
+	$endscreen/score.text = "Your Score: " + str(score) + "\n" + "High Score: " + str(high_score)
 
 
 func _input(event: InputEvent):
@@ -104,7 +136,7 @@ func _input(event: InputEvent):
 func _process(delta: float):
 	if game_running:
 		acorn_timer += delta
-		timer+=delta
+		timer += delta
 		stick_timer += delta
 		if timer >= spawning_interval:
 			timer = 0
@@ -121,29 +153,30 @@ func _process(delta: float):
 		else:
 			game_won = false
 			stop_game()
-		if score_target <= score:
+		if score >= score_target:
 			game_won = true
 			stop_game()
-		if stick_collected > 5:
+		if stick_collected >= 5:
 			game_won = false
 			stop_game()
 	$catchingnet.game_running = game_running
+
 func on_caught():
 	score += 1
 	$overlay/score.text = "Score: " + str(score)
 	$scored.play()
-	if score % 10 == 0 and score < 30:
-		spawning_interval -= 0.5
-		leaf_speed += 25
+	if score % 10 == 0 and score < 30 and not custom_game:
+		spawning_interval = max(0.5, spawning_interval - 0.5)
+		leaf_speed = min(MAX_LEAF_SPEED, leaf_speed + 25)
 
 func on_acorn_caught():
-	if $catchingnet.SPEED < 700:
+	if $catchingnet.SPEED < MAX_NET_SPEED:
 		$catchingnet.SPEED += 100
-		acorn_speed += 25
-		acorn_spawning_interval -= 0.5
-		
+	if not custom_game:
+		acorn_speed = min(MAX_ACORN_SPEED, acorn_speed + 25)
+		acorn_spawning_interval = max(3.0, acorn_spawning_interval - 0.5)
 	$scored.play()
-	score+=5
+	score += 5
 	$overlay/score.text = "Score: " + str(score)
 	
 func on_stick_caught():
@@ -151,6 +184,7 @@ func on_stick_caught():
 		score -= 1
 	stick_collected += 1
 	$overlay/score.text = "Score: " + str(score)
+	$overlay/sticks.text = "Sticks: " + str(stick_collected) + "/5"
 	$stick_collected.play()
 	
 func spawn_leaves():
@@ -165,7 +199,7 @@ func spawn_leaves():
 
 func spawn_acorns():
 	var acorn = acorn_scene.instantiate()
-	acorn.set("fall_speed", leaf_speed)
+	acorn.set("fall_speed", acorn_speed)
 	var screen_width = get_viewport_rect().size.x
 	acorn.position.x = randi_range(0, int(screen_width)) - int(global_position.x)
 	acorn.position.y = -1
@@ -188,3 +222,5 @@ func load_score():
 	if FileAccess.file_exists(save_path):
 		var file = FileAccess.open(save_path, FileAccess.READ)
 		high_score = file.get_var()
+	else:
+		high_score = 0
